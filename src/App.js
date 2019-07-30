d5765e61dcd89069e84e5435f58baee52e3e0ffd
Wendy <alteredco@gmail.com>
2019-07-30 05:44:40 -0700

    add lotteryBall component

--- src/App.js
+++ src/App.js
@@ -3,12 +3,11 @@ import Lottery from './Components/Lottery';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <Lottery
-      />
-    </div>
-  );
+    return (
+      <div className="App__container">
+        <Lottery />
+      </div>
+    )
 }
 
 export default App;
--- src/Components/Lottery.js
+++ src/Components/Lottery.js
@@ -1,7 +1,13 @@
 import React, {Component} from 'react';
 import './Lottery.css';
 
-class Lottery extends Component() {
+class Lottery extends Component{
+
+
+  // const title = title;
+  // const numBalls = ;
+  // const maxVal = {this.props.max};
+
   render(){
     return(
       <div>Hello</div>
